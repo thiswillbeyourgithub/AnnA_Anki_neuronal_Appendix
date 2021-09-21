39807e65e4f1498c52bed337784cd9e0633edfc4
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-09-21 22:27:38 +0200

    fix: nlimit

--- AnnA.py
+++ AnnA.py
@@ -966,8 +966,10 @@ plotting...")
         * note that you cannot use the pca version of the sBERT vectors
             otherwise you'd have to re run the whole PCA, so it's quicker
             to just use the full vectors
-        * nlimit is used to get the results by batch, but is multiplied by 10
-            if anki_or_print is set to "anki" for ease of use.
+        * note that if the results are displayed in the browser, the order 
+            cannot be maintained. So you will get the nlimit best match 
+            randomly displayed, then the nlimit+1 to 2*nlimit best match 
+            randomly displayed etc
         """
         pd.set_option('display.max_colwidth', None)
         df = self.df
@@ -1003,7 +1005,6 @@ plotting...")
                                      nlimit*cnt:nlimit*(cnt+1)
                                      ], ["text", "distance"]])
                 elif anki_or_print == "anki":
-                    nlimit *= 10
                     query = "cid:" + ",".join(
                             [str(x)
                                 for x in good_order[
