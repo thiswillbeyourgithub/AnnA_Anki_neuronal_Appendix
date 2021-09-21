76cfaa368e1f8abb16cac912c49837f46c9417e2
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-09-21 17:17:29 +0200

    field_mapping as an argument

--- AnnA.py
+++ AnnA.py
@@ -25,17 +25,6 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 signal.signal(signal.SIGINT, (lambda signal, frame: pdb.set_trace()))
 
 
-# This variable contains as key a notetype model name and as value the
-# name of the fields to keep, in order
-# ex : "basic": ["Front", "Back"]
-field_dic = {
-             "clozolkor": ["Header", "Body", "More"],
-             # shameless plug: clozolkor is my own template,
-             # more info: https://github.com/thiswillbeyourgithub/Clozolkor
-             "occlusion": ["Header", "Image"],
-             "spanish cards": ["Spanish", "English"]
-             }
-
 
 def asynchronous_importer():
     """
