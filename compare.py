4e98ecbad6c45e37c21aae6553d713cc47b9dd9b
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-09-27 13:19:14 +0200

    add compare.py

---
+++ compare.py
@@ -0,0 +1,13 @@
+
+def compare(s1, s2):
+    "used to simply compare two sentence"
+    from sentence_transformers import SentenceTransformer, util
+
+    # multilingual model:
+    #sBERT = SentenceTransformer('distiluse-base-multilingual-cased-v1')
+
+    # english only model:
+    sBERT = SentenceTransformer('all-MiniLM-L6-v2')
+
+    return util.cos_sim(sBERT.encode(s1),
+                        sBERT.encode(s2))
