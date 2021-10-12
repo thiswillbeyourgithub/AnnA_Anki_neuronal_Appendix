6a66e2b12f1342e6ab351d975c7b776070e829ad
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-10-13 01:46:31 +0200

    added more acronyms to the example

--- acronym_list_example.py
+++ acronym_list_example.py
@@ -9,4 +9,15 @@ acronym_dict = {
         "NN": "neural network",
         "NLP": "natural language processing",
         "LSTM": "Long Short Term Memory"
+        "LT": "lymphocyte T",
+        "LB": "lymphocyte B",
+        "NK": "lymphocyte T natural killer",
+        "Th17": "Lymphocyte T helper 17",
+        "Th1": "Lymphocyte T helper 1",
+        "Th2": "Lymphocyte T helper 2",
+        "TNM": "tumor lymph node metastasis",
+        "REM": "rapid eye movement sleep",
+        "HPV": "human papilloma virus",
+        "BRCA": "breast cancer gene",
+
 }
