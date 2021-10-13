43333467379beab867c12af95c7f7f29a9185a6b
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-10-13 13:23:20 +0200

    docs: tell that field mapping match loosely

--- field_mappings.py
+++ field_mappings.py
@@ -1,6 +1,8 @@
 # This variable contains as key a notetype model name and as value the
 # name of the fields to keep, in order
-# ex : "basic": ["Front", "Back"]
+# ex : "occlusion": ["Header", "Image"]
+# will match all templates that contain "occlusion" in their name
+
 field_dic = {
              "clozolkor": ["Header", "Body", "Hint", "More"],
              # shameless plug: clozolkor is my own template,
