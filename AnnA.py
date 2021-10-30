160de012c6f3cfb52069a50b458f5eec14fdbaab
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-10-30 16:58:43 +0200

    fix: stops were not properly tokenized

--- AnnA.py
+++ AnnA.py
@@ -925,8 +925,10 @@ using PCA...")
                 for lang in self.TFIDF_stopw_lang:
                     stops += stopwords.words(lang)
                 if self.TFIDF_tokenize:
-                    stops += [tknzer(x) for x in stops]
-                if self.TFIDF_stem:
+                    temp = []
+                    [temp.extend(tknzer(x)) for x in stops]
+                    stops.extend(temp)
+                elif self.TFIDF_stem:
                     stops += [ps.stem(x) for x in stops]
                 stops = list(set(stops))
             except Exception as e:
