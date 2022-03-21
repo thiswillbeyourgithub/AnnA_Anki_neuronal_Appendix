fb182dc4afead124e9d1ebe4f7c842f82d8117d5
thiswillbeyourgithub <github@32mail.33mail.comm>
2022-03-21 10:33:37 +0100

    fix: when formatting text

--- AnnA.py
+++ AnnA.py
@@ -619,11 +619,11 @@ less than threshold ({self.minimum_due}).\nStopping.")
 
         # if blockquote or li or ul, mention that it's a list item
         # usually indicating a harder card
-        if re.match("</blockquote>|</li>|</ul|", text, flags=re.M):
+        if re.match("</?blockquote/?>|</?li/?>|</?ul/?>|", text, flags=re.M):
             text += " list list list list list"
 
         # remove html spaces
-        text = re.sub('\\n|</?div>|<br>|</?span>|</?li>|</?ul>', " ", text)
+        text = re.sub('\\n|</?div/?>|<br/?>|</?span/?>|</?li/?>|</?ul/?>', " ", text)
         text = re.sub('</?blockquote(.*?)>', " ", text)
 
         # OCR
