27d3b70dadbfadb2b078e89155af6c9dd8eb4b9b
thiswillbeyourgithub <github@32mail.33mail.com>
2022-08-19 16:10:57 +0200

    Revert "new: stop using memoize and use joblib caching instead"
    This reverts commit 7f2684ffa0460ac12926a436f3ca9217bcf75479.

--- AnnA.py
+++ AnnA.py
@@ -651,6 +651,17 @@ threads of size {batchsize})")
                                   completer=auto_complete)
         return deckname
 
+    def memoize(self, f):
+        """ store previous value to speed up vector retrieval
+        (40x speed up) """
+        memo = {}
+
+        def helper(x):
+            if x not in memo:
+                memo[x] = f(x)
+            return memo[x]
+        return helper
+
     def _init_dataFrame(self):
         """
         create a pandas DataFrame with the information gathered from
@@ -1167,7 +1178,7 @@ threads of size {batchsize})")
                     assert len(ret) != 0
                     return ret
 
-                m_gIoF = self.mem.cache(get_index_of_fields)
+                m_gIoF = self.memoize(get_index_of_fields)
 
                 for notification in list(set(to_notify)):
                     red(notification)
