bb6951b10cb4eb64a6c7831ae3eb29f2ecf71b9e
thiswillbeyourgithub <github@32mail.33mail.com>
2022-10-06 11:31:56 +0200

    minor: change ankiconnect to companion addon in exception

--- AnnA.py
+++ AnnA.py
@@ -569,8 +569,8 @@ values. {e}")
                     'http://localhost:8775',
                     requestJson)))
         except (ConnectionRefusedError, urllib.error.URLError) as e:
-            beep(f"{e}: is Anki open and ankiconnect enabled?")
-            raise Exception(f"{e}: is Anki open and ankiconnect enabled?")
+            beep(f"{e}: is Anki open and 'AnnA Companion addon' enabled?")
+            raise Exception(f"{e}: is Anki open and 'AnnA Companion addon' enabled?")
 
         if len(response) != 2:
             beep('response has an unexpected number of fields')
