cdae313fa6ce498807f71af05eefc7a35eb5cd26
thiswillbeyourgithub <github@32mail.33mail.com>
2022-11-09 19:35:49 +0100

    new: set tokenizer model to both as default

--- AnnA.py
+++ AnnA.py
@@ -197,7 +197,7 @@ class AnnA:
                  # left for legacy reason
                  TFIDF_dim="auto",
                  TFIDF_tokenize=True,
-                 tokenizer_model="bert",
+                 tokenizer_model="both",
                  plot_2D_embeddings=False,
                  TFIDF_stem=False,
                  dist_metric="RBF",  # 'RBF' or 'cosine'
@@ -3121,11 +3121,11 @@ if __name__ == "__main__":
                             "one."))
     parser.add_argument("--tokenizer_model",
                         dest="tokenizer_model",
-                        default="bert",
+                        default="both",
                         metavar="TOKENIZER_MODEL",
                         required=False,
                         help=(
-                            "default to `bert`. Model to use for tokenizing "
+                            "default to `both`. Model to use for tokenizing "
                             "the text before running TFIDF. Possible values "
                             "are 'bert' and 'GPT' which correspond "
                             "respectivelly to `bert-base-multilingual-cased`"
