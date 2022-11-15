aabdffe031ccc30acbde47b7d723e45f8c5d483d
thiswillbeyourgithub <github@32mail.33mail.com>
2022-11-15 17:57:04 +0100

    new: added util folder and greek replacement file

--- AnnA.py
+++ AnnA.py
@@ -232,6 +232,8 @@ class AnnA:
 
         assert isinstance(
             replace_greek, bool), "Invalid type of `replace_greek`"
+        if replace_greek:
+            from util.greek import greek_alphabet_mapping
         self.replace_greek = replace_greek
 
         assert isinstance(keep_OCR, bool), "Invalid type of `keep_OCR`"
@@ -2799,59 +2801,6 @@ class ProgressParallel(joblib.Parallel):
         if abs(time.time() - PP._latest_progress_printed) >= 1:
             PP._pbar.refresh()
             PP._latest_progress_printed = time.time()
-
-
-# from https://gist.github.com/beniwohli/765262
-greek_alphabet_mapping = {
-    u'\u0391': 'Alpha',
-    u'\u0392': 'Beta',
-    u'\u0393': 'Gamma',
-    u'\u0394': 'Delta',
-    u'\u0395': 'Epsilon',
-    u'\u0396': 'Zeta',
-    u'\u0397': 'Eta',
-    u'\u0398': 'Theta',
-    u'\u0399': 'Iota',
-    u'\u039A': 'Kappa',
-    u'\u039B': 'Lamda',
-    u'\u039C': 'Mu',
-    u'\u039D': 'Nu',
-    u'\u039E': 'Xi',
-    u'\u039F': 'Omicron',
-    u'\u03A0': 'Pi',
-    u'\u03A1': 'Rho',
-    u'\u03A3': 'Sigma',
-    u'\u03A4': 'Tau',
-    u'\u03A5': 'Upsilon',
-    u'\u03A6': 'Phi',
-    u'\u03A7': 'Chi',
-    u'\u03A8': 'Psi',
-    u'\u03A9': 'Omega',
-    u'\u03B1': 'alpha',
-    u'\u03B2': 'beta',
-    u'\u03B3': 'gamma',
-    u'\u03B4': 'delta',
-    u'\u03B5': 'epsilon',
-    u'\u03B6': 'zeta',
-    u'\u03B7': 'eta',
-    u'\u03B8': 'theta',
-    u'\u03B9': 'iota',
-    u'\u03BA': 'kappa',
-    u'\u03BB': 'lamda',
-    u'\u03BC': 'mu',
-    u'\u03BD': 'nu',
-    u'\u03BE': 'xi',
-    u'\u03BF': 'omicron',
-    u'\u03C0': 'pi',
-    u'\u03C1': 'rho',
-    u'\u03C3': 'sigma',
-    u'\u03C4': 'tau',
-    u'\u03C5': 'upsilon',
-    u'\u03C6': 'phi',
-    u'\u03C7': 'chi',
-    u'\u03C8': 'psi',
-    u'\u03C9': 'omega',
-}
 
 
 if __name__ == "__main__":
