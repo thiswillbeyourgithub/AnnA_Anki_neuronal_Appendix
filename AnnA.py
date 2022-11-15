f35bcf18bfe1c8751d887ba8e4dc1b969a16c5d4
thiswillbeyourgithub <github@32mail.33mail.com>
2022-11-15 14:03:52 +0100

    ignore missing_ocr tag by default

--- AnnA.py
+++ AnnA.py
@@ -180,7 +180,7 @@ class AnnA:
                  replace_greek=True,
                  keep_OCR=True,
                  append_tags=True,
-                 tags_to_ignore=["AnnA"],
+                 tags_to_ignore=["AnnA", "missing_OCR"],
                  tags_separator="::",
                  add_KNN_to_field=False,
                  filtered_deck_name_template=None,
@@ -3116,7 +3116,7 @@ if __name__ == "__main__":
                         nargs="*",
                         metavar="TAGS_TO_IGNORE",
                         dest="tags_to_ignore",
-                        default="[AnnA]",
+                        default="[AnnA, missing_OCR]",
                         type=str,
                         required=False,
                         help=(
@@ -3125,7 +3125,7 @@ if __name__ == "__main__":
                             "appending tags to cards. This is not a "
                             "list of tags "
                             "whose card should be ignored! Default is "
-                            "'[AnnA]'. Set to None to disable it."))
+                            "'[AnnA,missing_OCR]'. Set to None to disable it."))
     parser.add_argument("--tags_separator",
                         nargs=1,
                         metavar="TAGS_SEP",
