4fee7b6d5c2ec3a417b3793224c8069d80635485
thiswillbeyourgithub <github@32mail.33mail.com>
2023-05-03 18:32:46 +0200

    fix: replace acronym was broken :/

--- AnnA.py
+++ AnnA.py
@@ -1228,7 +1228,7 @@ class AnnA:
                                       self._regexp_acronym_replacer(in_string,
                                                                     regex,
                                                                     new_value),
-                                      text)
+                                      text, count=1)
 
                     # if overlapping patterns, apply sequentially
                     if regex.pattern in self.acronyms_overlapping:
@@ -1238,7 +1238,7 @@ class AnnA:
                                           lambda in_string:
                                           self._regexp_acronym_replacer(
                                               in_string, regex2, new_value2),
-                                          text)
+                                          text, count=1)
                             already_replaced.append(regex2)
 
         # misc
