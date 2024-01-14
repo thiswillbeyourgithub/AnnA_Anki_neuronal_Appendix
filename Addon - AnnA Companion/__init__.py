90173a7bbecba5ac62062f2ec42814eb5688d5a2
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-01-14 14:23:55 +0100

    fix: seting due order of cards not in filtered deck would lose the correct due date

--- Addon - AnnA Companion/__init__.py
+++ Addon - AnnA Companion/__init__.py
@@ -441,6 +441,9 @@ class AnkiConnect:
             order += 1
             try:
                 ankiCard = self.getCard(card)
+                if ankiCard.due >= -10_000:
+                    result.append([False, f"Due value was {ankiCard.due}"])
+                    continue
                 ankiCard.due = order
                 ankiCard.flush()
                 result.append([True])
